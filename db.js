2061eb62a32566ac3316f2db3633626500457cf0
juan jose londoño <juanjoselondonodavid@gmail.com>
2023-01-05 21:42:07 -0500

    /public deleted

--- db.js
+++ db.js
@@ -1,11 +1,8 @@
 const dotenv = require('dotenv').config({path:`${__dirname}/config.env`});
-const host = process.env.DB_HOST;
-const username = process.env.DB_USER;
-const password = process.env.DB_PASS;
 const url = process.env.DB_URL;
-const messages = `${host}${username}${password}${url}/message`;
+const messages = `${url}/message`;
 const db = require('mongoose');
-
+console.log(messages)
 db.Promise = global.Promise;
 async function connect() {
     await db.connect(messages, {
